720fdb60af011f05404c88efdef5f5f1278b0638
Sameer Kumar <kumarsameer@seasiaconnect.com>
2025-03-13 16:08:12 +0530

    changes

--- ApiPro/RestApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    # path('',views.student,name="student"),
-
-    # api endpoints
-    path('student',views.StudentsView,name="student")
-
-]
